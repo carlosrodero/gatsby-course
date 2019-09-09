6d3de6c29f0aee141661387a1df5feb0fe64ffcf
Calos Rodero <developer.carodero@gmail.com>
2019-09-09 00:55:56 -0300

    completed

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,16 @@
+const links = [
+  {
+    label: "Github",
+    url: "https://github.com/carlosrodero",
+  },
+  {
+    label: "Twitter",
+    url: "https://twitter.com/carlosrodero_",
+  },
+  {
+    label: "Instagram",
+    url: "https://instagram.com/carlosrodero_caca",
+  },
+]
+
+export default links
